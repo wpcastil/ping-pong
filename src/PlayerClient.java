2cb395117273e4ad25cf5ec5a4b20785a382153c
Wiliam Felisiak Passaglia Castilhos <5400224+wpcastil@users.noreply.github.com>
2018-07-18 14:51:31 -0300

    Update PlayerClient.java

--- src/PlayerClient.java
+++ src/PlayerClient.java
@@ -2,9 +2,7 @@ import java.io.Serializable;
 
 
 public class PlayerClient implements Serializable {
-	 	
-	private static final long serialVersionUID = 1L;
-	
+	 		
 	private String name ="";
 	private int	x, y;
     	boolean ok = false;
